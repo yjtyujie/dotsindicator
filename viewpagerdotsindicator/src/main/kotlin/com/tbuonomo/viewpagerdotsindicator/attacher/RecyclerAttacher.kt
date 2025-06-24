5f7a6af1a6463f4dcc31d77720de42286f406686
yujie <tanyj@bookdoc.com>
2025-06-24 18:13:26 +0800

    Add RecyclerAttacher

--- viewpagerdotsindicator/src/main/kotlin/com/tbuonomo/viewpagerdotsindicator/attacher/RecyclerAttacher.kt
+++ viewpagerdotsindicator/src/main/kotlin/com/tbuonomo/viewpagerdotsindicator/attacher/RecyclerAttacher.kt
@@ -5,8 +5,9 @@ import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.tbuonomo.viewpagerdotsindicator.BaseDotsIndicator
 import com.tbuonomo.viewpagerdotsindicator.OnPageChangeListenerHelper
+import kotlin.math.max
 
-private class RecyclerAttacher(val snapHelper: PagerSnapHelper) :
+internal class RecyclerAttacher(val snapHelper: PagerSnapHelper) :
     DotsIndicatorAttacher<RecyclerView, RecyclerView.Adapter<*>>() {
     override fun getAdapterFromAttachable(attachable: RecyclerView): RecyclerView.Adapter<RecyclerView.ViewHolder>? =
         attachable.adapter
@@ -55,6 +56,7 @@ private class RecyclerAttacher(val snapHelper: PagerSnapHelper) :
     ): BaseDotsIndicator.Pager {
         return object : BaseDotsIndicator.Pager {
             var onScrollListener: RecyclerView.OnScrollListener? = null
+            val initialDx = 0 // this is supposed to be the scroll position of the recyclerview when you initialize the helper. I think
 
             override val isNotEmpty: Boolean
                 get() = adapter.itemCount > 0
@@ -78,19 +80,54 @@ private class RecyclerAttacher(val snapHelper: PagerSnapHelper) :
                 onScrollListener?.let(attachable::removeOnScrollListener)
             }
 
+//            override fun addOnPageChangeListener(
+//                onPageChangeListenerHelper: OnPageChangeListenerHelper
+//            ) {
+//                onScrollListener = object : RecyclerView.OnScrollListener() {
+//                    override fun onScrolled(attachable: RecyclerView, dx: Int, dy: Int) {
+//                        snapHelper.findSnapView(attachable.layoutManager)
+//                            ?.let { attachable.layoutManager?.getPosition(it) }
+//                            ?.let { snapPosition ->
+//                                onPageChangeListenerHelper.onPageScrolled(
+//                                    snapPosition,
+//                                    dx.toFloat()
+//                                )
+//                            }
+//                    }
+//                }
+//                attachable.addOnScrollListener(onScrollListener!!)
+//            }
+
             override fun addOnPageChangeListener(
                 onPageChangeListenerHelper: OnPageChangeListenerHelper
             ) {
                 onScrollListener = object : RecyclerView.OnScrollListener() {
+                    private var currentDx = initialDx
+                    private var currentPosition: Int? = null
+
                     override fun onScrolled(attachable: RecyclerView, dx: Int, dy: Int) {
-                        snapHelper.findSnapView(attachable.layoutManager)
-                            ?.let { attachable.layoutManager?.getPosition(it) }
-                            ?.let { snapPosition ->
-                                onPageChangeListenerHelper.onPageScrolled(
-                                    snapPosition,
-                                    dx.toFloat()
-                                )
+                        this.currentDx += dx
+
+                        val view = snapHelper.findSnapView(attachable.layoutManager)
+                        if (view != null) {
+                            val width = view.width
+
+                            if (currentPosition == null && initialDx > width) {
+                                setCurrentItem(max(this.currentDx / width, count), false) // reset
                             }
+
+                            val newPosition = this.currentDx / width
+                            this.currentPosition = newPosition
+
+                            onPageChangeListenerHelper.onPageScrolled(
+                                newPosition,
+                                try {
+                                    (this.currentDx % width).toFloat() / width
+                                } catch (e: ArithmeticException) {
+                                    0f
+                                }
+                            )
+                        }
                     }
                 }
                 attachable.addOnScrollListener(onScrollListener!!)
